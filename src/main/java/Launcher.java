ada09ec5a16cdd7e868dbb48d7270008d966e895
Maksim Lazeba <m.lazeba@gmail.com>
2017-12-06 19:48:41 +0200

    Example with keyboard

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -5,6 +5,10 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.model.request.ChatAction;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendChatAction;
 import com.pengrad.telegrambot.request.SendMessage;
 
 public class Launcher {
@@ -28,14 +32,34 @@ public class Launcher {
       System.out.println("Message from " + from);
       String text = update.message().text();
       System.out.println(text);
-      if (text != null && text.toLowerCase().contains("hello")) {
-        greetUser(from);
+      if (text == null) {
+        continue;
       }
+      String textProcessed = text.toLowerCase();
+      if (textProcessed.contains("hello")) {
+        greetUser(from);
+      } else if (textProcessed.contains("show")) {
+        choice(from);
+      }
+
     }
   }
 
   private static void greetUser(User user) {
+    bot.execute(new SendChatAction(user.id(), ChatAction.typing));
     bot.execute(new SendMessage(user.id(), "Hello, " + user.firstName() + "!"));
+  }
+
+  private static void choice(User user) {
+    Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(
+        new String[]{"first row button1", "first row button2"},
+        new String[]{"second row button1", "second row button2"})
+        .oneTimeKeyboard(true)   // optional
+        .resizeKeyboard(true)    // optional
+        .selective(true);        // optional
+
+    bot.execute(new SendMessage(user.id(), "Wanna more? Keyboard example")
+        .replyMarkup(replyKeyboardMarkup));
   }
 
 }
